02f254a45761defb7c03d9b44b3688a969c81cd7
Elisandro Santolin <elisandro.santolin@outlook.com>
2024-11-12 20:29:42 -0300

    Feat: files challenges alura backend immersion logic

---
+++ challenger/challengeOne.js
@@ -0,0 +1,51 @@
+alert('Boas vindas ao nosso site!');
+
+let name = 'Lua';
+let age = 25;
+
+let numberSales = 50;
+let balanceAvailable = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let warningMessage = 'Erro! Preencha todos os campos';
+alert(warningMessage);
+
+
+let userName = prompt('Digite seu nome: ');
+let userAge = prompt('Digite sua idade: ');
+
+if(userAge >= 18){
+    alert('Pode tirar a habilitação!');
+
+}else {
+    alert('Menor de idade!');
+}
+
+
+
+
+
+//Desafios
+
+// - Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+
+// - Declare uma variável chamada nome e atribua a ela o valor "Lua".
+
+// - Crie uma variável chamada idade e atribua a ela o valor 25.
+
+// - Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+
+// - Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+
+// - Exiba um alerta com o texto "Erro! Preencha todos os campos"
+
+// - Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+
+// - Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+
+// - Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+
+// - Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+
+
